1975962f939c0959e9623ca76e755820ba296ef5
Jan Suchotzki <jan@suchotzki.de>
2015-07-02 10:32:07 +0200

    Erste Version

---
+++ C_DateienErstellen/Programm.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (void)
+{
+  FILE *dateiZeiger;
+  int anzahlZeichenGeschrieben = 0;
+
+  printf("\nBeispielprogramm von Lernmoment.de. Zeigt wie Dateien geöffnet und beschrieben werden.\n");
+
+  // Öffnen der Datei im Schreibmodus.
+  // Bei "w" wird der Inhalt der Datei gelöscht, wenn sie existiert, oder sie wird erstellt, 
+  // wenn sie nicht existiert.
+  dateiZeiger = fopen("lernmoment.txt", "w");
+
+  // Überprüfen ob die Datei geöffnet werden konnte. Vielleicht besteht die Datei schon mit einem
+  // Schreibschutz, oder der Aufrufer hat keine Berechtigung um die Datei zu erstellen.
+  if (dateiZeiger == NULL)
+  {
+    printf("Die Datei konnte nicht geöffnet werden!");
+    return EXIT_FAILURE;
+  }
+
+  // Nun kann etwas in die Datei geschrieben werden. Erst wird der Inhalt von fprintf in einen
+  // Zwischenspeicher geschrieben. Erst wenn die Datei geschlossen wurde (fclose), ist der
+  // Inhalt auch wirklich drin.
+  anzahlZeichenGeschrieben = fprintf(dateiZeiger, "Lernmoment - Dateien erstellen. Geschafft!\n");
+  fclose(dateiZeiger);
+
+  if (anzahlZeichenGeschrieben > 0)
+  {
+	printf("Glückwunsch! Du hast gerade erfolgreich %i Zeichen in die Datei geschrieben.\n", anzahlZeichenGeschrieben);
+  }
+
+  return EXIT_SUCCESS;
+}
---
+++ C_DateienErstellen/README.md
@@ -0,0 +1,17 @@
+#Lernmoment in C - Dateien, wie du Inhalte in eine neue Datei bekommst
+
+Ähnlich wie bei printf, können Dateien dir in sehr vielen Bereichen helfen. 
+
+Hinweis: Zum erstellen und verwenden von Dateien werden Zeiger benötigt. Dies ist schon eher ein Thema für Fortgeschrittene. Du kannst trotzdem die Beispiele verwenden und damit ein bisschen rum spielen. Gerade das beschreiben von Dateien ist sehr einfach und kann dir an vielen Stellen helfen.
+
+**Merke:**
+
+-	
+
+**Lernquiz:** Verwende folgende Fragen um das gelernte von heute zu festigen:
+
+-	
+
+Am besten du schaust dir morgen und dann nochmal in ein paar Tagen die vorherigen Fragen an und beantwortest sie ohne den Text vorher gelesen zu haben.
+
+**Weitere Informationen:** Den kompletten Quellcode zum heutigen Lernmoment findest du [hier](https://github.com/inginform/lernmomente/tree/master/C_DateienErstellen). Eine umfangreichere Erklärung gibt es bei [wikibooks](https://de.wikibooks.org/wiki/C-Programmierung:_Dateien).
