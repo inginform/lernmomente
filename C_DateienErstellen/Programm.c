f9d109bdcb52b2c36823fc3d339008f0a5b01ac9
Jan Suchotzki <jan@suchotzki.de>
2015-07-02 16:23:36 +0200

    Benennt dateiZeiger in datenstrom um. Ist konsistenter mit Artikel.

--- C_DateienErstellen/Programm.c
+++ C_DateienErstellen/Programm.c
@@ -3,7 +3,7 @@
 
 int main (void)
 {
-  FILE *dateiZeiger;
+  FILE *datenstrom;
   int anzahlZeichenGeschrieben = 0;
 
   printf("\nBeispielprogramm von Lernmoment.de. Zeigt wie Dateien geöffnet und beschrieben werden.\n");
@@ -11,11 +11,11 @@ int main (void)
   // Öffnen der Datei im Schreibmodus.
   // Bei "w" wird der Inhalt der Datei gelöscht, wenn sie existiert, oder sie wird erstellt, 
   // wenn sie nicht existiert.
-  dateiZeiger = fopen("lernmoment.txt", "w");
+  datenstrom = fopen("lernmoment.txt", "w");
 
   // Überprüfen ob die Datei geöffnet werden konnte. Vielleicht besteht die Datei schon mit einem
   // Schreibschutz, oder der Aufrufer hat keine Berechtigung um die Datei zu erstellen.
-  if (dateiZeiger == NULL)
+  if (datenstrom == NULL)
   {
     printf("Die Datei konnte nicht geöffnet werden!");
     return EXIT_FAILURE;
@@ -24,8 +24,8 @@ int main (void)
   // Nun kann etwas in die Datei geschrieben werden. Erst wird der Inhalt von fprintf in einen
   // Zwischenspeicher geschrieben. Erst wenn die Datei geschlossen wurde (fclose), ist der
   // Inhalt auch wirklich drin.
-  anzahlZeichenGeschrieben = fprintf(dateiZeiger, "Lernmoment - Dateien erstellen. Geschafft!\n");
-  fclose(dateiZeiger);
+  anzahlZeichenGeschrieben = fprintf(datenstrom, "Lernmoment - Dateien erstellen. Geschafft!\n");
+  fclose(datenstrom);
 
   if (anzahlZeichenGeschrieben > 0)
   {
