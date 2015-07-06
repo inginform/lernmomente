a3fe7eec7eb7865bf1d554acdc8fae8ae9662d51
Jan Suchotzki <jan@suchotzki.de>
2015-07-07 00:32:02 +0200

    Fuegt erklaerenden Kommentar hinzu.

--- C_IncludeWaechter/Pragma.h
+++ C_IncludeWaechter/Pragma.h
@@ -1,3 +1,8 @@
+// Viele Compiler unterstützen eine Anweisung die nicht im C-Standard definiert ist. 
+// Das ist das #pragma once. Es hat den gleichen Effekt wie ein Include Guard, aber ist einfacher
+// zu schreiben und eliminiert einige Fehlerquellen. Insbesondere bezüglich des Namens des
+// Include Guards.
+// Allerdings gibt es durchaus Compiler im Embedded Bereich, die diese Anweisung nicht kennen.
 #pragma once
 
 #include "Guard.h"
